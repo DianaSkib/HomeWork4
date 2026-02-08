6bdbeedbda5e82dc2c5a6a3fe2d62001c1dd138e
Диана <ashkhar.ik@mail.ru>
2026-02-08 21:04:44 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /// #1
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        }
 
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + ", он не достиг совершеннолетия, нужно подождать.");
         }
 
     }
+}
