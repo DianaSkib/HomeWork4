329d03a8dc7ee772b95ea5afc11e3fb01e8af883
Диана <ashkhar.ik@mail.ru>
2026-02-10 22:27:12 +0300

    Task7

--- src/Main.java
+++ src/Main.java
@@ -61,5 +61,17 @@ public class Main {
         } else if (peopleInVan > seatingPositions && peopleInVan > standingPositions) {
             System.out.println("Вагон полностью занят.");
         }
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+        if (one > two && one < three) {
+            System.out.println("one наибольшее число.");
+        } else if (two > three) {
+            System.out.println("two наибольшее число.");
+        } else {
+            System.out.println("three наибольшее число.");
+        }
     }
-    }
+}
